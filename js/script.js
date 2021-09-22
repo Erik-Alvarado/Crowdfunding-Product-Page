c492e01c27c5f9538a22f6613e70485b9738cc52
Tofu <e.alvarado@meditrial.net>
2021-09-22 16:54:43 -0400

    continued to add functionality to js and simplified code

--- js/script.js
+++ js/script.js
@@ -1,31 +1,51 @@
-const bookmark = document.querySelector('.mark-icon');
-const backer = document.querySelector('.back-project');
-const selection = document.querySelectorAll('.select-option');
-const hamMenu = document.querySelector('.ham-menu');
-const navbar = document.querySelector('.navbar');
-const overlay = document.querySelector('#page-mask');
-const modalContain = document.querySelector('.modal-container');
-const modalSuccess = document.querySelector('.modal-success');
-const closeModal = document.querySelector('.close');
+const bookmark = document.querySelector(".mark-icon");
+const backer = document.querySelector(".back-project");
+const hamMenu = document.querySelector(".ham-menu");
+const navbar = document.querySelector(".navbar");
+const overlay = document.querySelector("#page-mask");
+const modalContain = document.querySelector(".modal-container");
+const modalSuccess = document.querySelector(".modal-success");
+const closeModal = document.querySelectorAll(".close");
+const advance = document.querySelectorAll(".continue-btn");
+const modalSelect = document.querySelector(".modal-select");
 
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-});
-
-bookmark.addEventListener("click", () => {
-    bookmark.classList.toggle('booked');
-})
+//event listeners
 
 backer.addEventListener("click", () => {
-    backer.classList.toggle('open');
-    modalContain.classList.toggle('active');
-    overlay.classList.toggle('active');
-    modalSuccess.classList.add('hide');
-})
+  backer.classList.toggle("open");
+  overlayActive();
+  continuePledge();
+  modalSuccess.classList.add("hide");
+});
 
-closeModal.addEventListener("click", () => {
-    modalContain.classList.remove('active');
-    overlay.classList.remove('active');
-})
+hamMenu.addEventListener("click", hamburgerToggle);
+
+closeModal.forEach((closer) => closer.addEventListener("click", overlayActive));
+
+advance.forEach((forward) => forward.addEventListener("click", continuePledge));
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("booked");
+});
+
+//functions
+
+function hamburgerToggle() {
+  hamMenu.classList.toggle("active");
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+function continuePledge() {
+  if (modalSuccess.classList.contains("hide")) {
+    modalSuccess.classList.remove("hide");
+    modalSelect.classList.add("hide");
+  } else {
+    modalSuccess.classList.add("hide");
+    modalSelect.classList.remove("hide");
+  }
+}
+
+function overlayActive() {
+  modalContain.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
